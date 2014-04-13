40510d0a4620f421ae4fb2c2e4bdcfb4589f0c29
michaelrhodes <spam@michaelrhod.es>
2014-04-13 10:51:12 +1000

    Updated some styles. Updated mkdom. Fixed fs.readFileSync calls.

--- server/views/base.js
+++ server/views/base.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var mkdom = require('mkdom')
 
 var Base = function() {
-  var template = fs.readFileSync(__dirname + '/template.html')
+  var template = fs.readFileSync(__dirname + '/template.html', 'utf8')
   this.dom = mkdom(template)
   this.body = this.dom.querySelector('body') 
   this.header = this.dom.querySelector('header') 
--- server/views/list.js
+++ server/views/list.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var shared = require('../../shared/list')
 
 var List = function() {
-  var template = fs.readFileSync('./shared/list.html')
+  var template = fs.readFileSync('./shared/list.html', 'utf8')
   this.init(template)
 }
 
